32a0c5fa507b4d104ca413e5c8934ba700e5bb87
Jimmy Jones <jimjones26@gmail.com>
2016-09-03 23:44:41 -0400

    refactor the master/detail view into separate components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-
-import { AppComponent } from './app.component';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { AppComponent }         from './app.component';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule
     ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
