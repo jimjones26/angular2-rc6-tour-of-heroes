7838b50913b9ad271566eeb29c42999275d77d40
Jimmy Jones <jimjones26@gmail.com>
2016-09-06 23:04:22 -0400

    add a hero search feature

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,4 @@
+import './rxjs-extensions';
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { FormsModule }      from '@angular/forms';
@@ -11,6 +12,7 @@ import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './hero-search.component'
 import { HeroService }          from './hero.service';
 import { routing }              from './app.routing';
 
@@ -26,7 +28,8 @@ import { routing }              from './app.routing';
         AppComponent,
         DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        HeroSearchComponent
     ],
     providers: [
         HeroService
